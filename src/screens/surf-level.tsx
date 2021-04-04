67872e4a1e6a8d05b8d2333f2b6787a84ea6b4b7
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2021-04-04 20:49:37 +0200

    build: add styling at SurfLevelScreen

--- src/screens/surf-level.tsx
+++ src/screens/surf-level.tsx
@@ -14,20 +14,50 @@ type Props = { navigation: SurfLevelScreenNavProp };
 const SurfLevelScreen: React.FC<Props> = ({ navigation }: Props) => {
   return (
     <>
-      <View style={styles.wrapper}>
-        <Text style={styles.headerText}>Ask surf level here</Text>
+      <View style={styles.headerWrapper}>
+        <Text style={styles.headerText}>{"Tell us about your surf level"}</Text>
+      </View>
+      <View style={styles.surfLevelOptionsWrapper}>
+        <View style={styles.buttonWrapper}>
+          <Text>{"Beginner Button"}</Text>
+        </View>
+        <View style={styles.buttonWrapper}>
+          <Text>{"Beginner Button"}</Text>
+        </View>
+        <View style={styles.buttonWrapper}>
+          <Text>{"Beginner Button"}</Text>
+        </View>
+        <View style={styles.buttonWrapper}>
+          <Text>{"Beginner Button"}</Text>
+        </View>
+      </View>
+      <View style={styles.nextStepButtonWrapper}>
+        <Text>{"Next step button"}</Text>
       </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  wrapper: {
+  headerWrapper: {
     ...Container.centerAlignedContainer,
-    paddingTop: 120,
+    flex: 1,
   },
   headerText: {
     ...Typography.h1,
+  },
+  surfLevelOptionsWrapper: {
+    ...Container.centerAlignedContainerVertical,
+    paddingTop: "2.5%",
+  },
+  buttonWrapper: {
+    ...Container.centerAlignedContainer,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  nextStepButtonWrapper: {
+    ...Container.centerAlignedContainer,
+    flex: 1,
   },
 });
 
