e7064406da66fdb054936513d6a1224b4a766cfa
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2021-04-10 11:41:03 +0200

    chore: add @types/react-test-renderer as dev dependency

--- src/components/go-back-button/__test__/go-back-button.test.tsx
+++ src/components/go-back-button/__test__/go-back-button.test.tsx
@@ -3,9 +3,9 @@ import renderer from "react-test-renderer";
 import GoBackButton from "../go-back-button";
 
 describe("Test GoBackButton component", () => {
-    it("should renders GoBackButton component correctly", () => {
-        const tree = renderer.create(<GoBackButton />).toJSON();
+  it("should renders GoBackButton component correctly", () => {
+    const tree = renderer.create(<GoBackButton />).toJSON();
 
-        expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
+  });
 });
